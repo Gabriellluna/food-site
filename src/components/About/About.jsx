78c2b5768ae42cce056619eeadeda02f2921fcdb
Gabriel Luna <glunamaia@gmail.com>
2026-02-05 18:15:13 -0300

    feat: ancoragem da seção de navegação e início do desenvolvimento da seção sobre nós na Home

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,11 +2,39 @@ import React from 'react';
 import './About.css'
 
 const About = () => {
+
+  const section = document.querySelector('.cut-section');
+  const image = document.querySelector('.cut-image');
+
+  window.addEventListener('scroll', () => {
+  const rect = section.getBoundingClientRect();
+  const windowHeight = window.innerHeight;
+
+  if (rect.top < windowHeight && rect.bottom > 0) {
+    const progress =
+      (windowHeight - rect.top) / (windowHeight + rect.height);
+
+    const translateY = Math.min(Math.max(progress * 100, 0), 100);
+
+    image.style.transform = `translateY(${translateY}px)`;
+    }
+  });
+
+
   return (
     <>
-      <div className='about-container'>
+      <section class="cut-section d-flex flex-direction-column">
+        <h1 style={{color:"#000000"}}>texto bonito aqui</h1>
 
+      <div class="cut-mask">
+        <img 
+          src="images/dish3.jpg" 
+          alt="Sushi" 
+          class="cut-image"
+        />
       </div>
+    </section>
+
     </>
   );
 };
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {Swiper, SwiperSlide} from 'swiper/react'
 import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper/modules';
 import 'swiper/css';
@@ -6,11 +6,32 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 import './header.css'
+import { useSearchParams } from 'react-router-dom';
 
 const Header = () => {
+  
+  const [show, setShow] = useState(false)
+  const header = document.getElementById('container');
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+    if (window.scrollY > header ) {
+      setShow(true)
+    } else {
+      setShow(false)
+    }
+  });
+
+  }, [])
+
+
+    useEffect(() => {
+    console.log(show)
+   })
+
   return (
     <>
-      <div className='container'>
+      <div className='container' id='container'>
         <Swiper
         className='cards'
         modules={[Navigation, Pagination, Autoplay]}
@@ -29,7 +50,7 @@ const Header = () => {
           </SwiperSlide>
         </Swiper>
         <h1 className='slogan'>Precisão no corte, excelência no sabor</h1>
-        <div className='navigation justify-content-between'>
+        <div className={`navigation justify-content-between ${show === true ? 'navigation--solid' : null}`}>
           <img alt='logo aqui' src='images/logo.png'></img>
           <div className='nav'>
             <h3>Home</h3>
