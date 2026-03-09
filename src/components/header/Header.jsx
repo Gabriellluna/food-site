33fe2eca2b3bec89f3eed22fe05876e265e81f21
Gabriel Luna <glunamaia@gmail.com>
2026-03-09 12:54:53 -0300

    feat: menu mobile

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import {Swiper, SwiperSlide} from 'swiper/react'
+import { Swiper, SwiperSlide } from 'swiper/react'
 import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -9,34 +9,34 @@ import './header.css'
 import { useSearchParams } from 'react-router-dom';
 
 const Header = () => {
-  
+
   const [show, setShow] = useState(false)
   const vh100 = window.innerHeight;
+  const [menuActive, setMenuActive] = useState(false)
 
   useEffect(() => {
     window.addEventListener('scroll', () => {
-    if (window.scrollY > vh100 ) {
-      setShow(true)
-    } else {
-      setShow(false)
-    }
-  });
+      if (window.scrollY > vh100) {
+        setShow(true)
+      } else {
+        setShow(false)
+      }
+    });
 
   }, [])
 
-
-    useEffect(() => {
-    console.log(show)
-   })
+  const setActive = () => {
+    setMenuActive(prev => !prev)
+  };
 
   return (
     <>
       <div className='container' id='container'>
         <Swiper
-        className='cards'
-        modules={[Navigation, Pagination, Autoplay]}
-        slidesPerView={1}
-        autoplay={{delay:3000}}
+          className='cards'
+          modules={[Navigation, Pagination, Autoplay]}
+          slidesPerView={1}
+          autoplay={{ delay: 3000 }}
         >
           <SwiperSlide className='card'>
             <div >
@@ -58,6 +58,25 @@ const Header = () => {
             <a href='/cardapio'><h3 className='text-white'>Cardápio</h3></a>
             <h3 className='text-white'>Rodízio</h3>
             <h3 className='text-white'>Contato</h3>
+          </div>
+          <div className='menu-mobile-container'>
+            <div className="menu-mobile" onClick={setActive} style={{display: `${menuActive ? "none" : "block"}`}}>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div className={`nav-list ${menuActive ? 'open' : ''}`}>
+              <div
+                className="list"
+                style={{ animationDelay: ".5s" }}
+              >
+                <a href="/"><h3 className='text-white'>Home</h3></a>
+                <a href='/sobre'><h3 className='text-white'>Sobre</h3></a>
+                <a href='/cardapio'><h3 className='text-white'>Cardápio</h3></a>
+                <h3 className='text-white'>Rodízio</h3>
+                <h3 className='text-white'>Contato</h3>
+              </div>
+            </div>
           </div>
         </div>
       </div>
